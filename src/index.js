c0b9c663244fe441f2ee59cf0c28f48d7f33427c
gvjas <gvjasch@gmail.com>
2020-10-05 13:28:05 +0900

    feat: solution for task Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+    const nameNumber = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+                        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const nameTensNumber = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    let res;
+    if (number === 0) {
+        res = 'zero'
+    } else if (number < 20) {
+        res = nameNumber[number];
+    } else {
+        res = `${number / 1000 < 1000 ? '' : nameNumber[parseInt(number / 1000)] + ' thousand'}
+                ${number % 1000 < 100 ? '' : nameNumber[parseInt(number % 1000 / 100)] + ' hundred'}
+                ${number % 100 < 20 ? nameNumber[number % 100] : nameTensNumber[parseInt(number % 100 / 10)]}
+                ${number % 100 < 20 ? '' : nameNumber[number % 10]}`;
+ 
+    }
+    res = res.replace(/\s+/g, ' ').trim()
+    return res;
 }
